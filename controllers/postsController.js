f034d4f5d2fc526cd4f51f9e4c47d9a16ab9daee
dariosanacuore <dariosanacuore2@gmail.com>
2026-01-16 16:18:17 +0100

    feat.Aggiunta la funzione show

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -19,15 +19,29 @@ function index(req, res) {
     });
 }
 
-
 function show(req, res) {
     const id = parseInt(req.params.id);
-    const post = posts.find(p => p.id === id);
-    if (!post) {
-        return res.status(404).json({ error: "Post non trovato" });
-    }
-    res.json(post);
+
+    const sql = "SELECT * FROM posts WHERE id = ?";
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({
+                error: "Errore del database"
+            });
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({
+                error: "Post non trovato"
+            });
+        }
+
+        res.json(results[0]);
+    });
 }
+
 
 function store(req, res) {
     const dati = req.body;
